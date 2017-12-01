72480f2741b53dd174a2a3ef34a0c6bf34b765b1
Justin J Medina <jj.medina@runtriz.com>
2017-12-01 12:47:30 -0800

    set up to parse json

--- server/app.js
+++ server/app.js
@@ -3,17 +3,20 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function (request, response) {
   response.sendStatus(200);
 });
 
-app.post('/newPoint', function (request, response) {
-  /* request will be a JSON array of point objects, each having an x, y, and z attribute */
-  const points = request;
-
+/* request will be a JSON array of point objects, each having an x, y, and z attribute */
+app.post('/sendPoints', function (request, response) {
   // stream the points down to any subscribed clients
-  res.sendStatus(200);
+  io.emit('points', request.body);
+  response.sendStatus(200);
 });
 
 io.on('connection', function (socket) {
